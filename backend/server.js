94a0bcb2f5fc957d49ee65e8bf6d61034dd1457a
Tharan <tharan90941@gmail.com>
2026-08-19 18:56:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const reportRoutes = require('./routes/reports');
 const adminRoutes = require('./routes/admin');
 
 const app = express();
+app.set('trust proxy', 1);
 
 app.use(cors({ origin: process.env.CLIENT_ORIGIN || 'http://localhost:3000' }));
 app.use(express.json());
